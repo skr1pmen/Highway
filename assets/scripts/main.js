a749583a4d784ac4f036b7687776d6838c2c1058
skr1pmen <KIberKot.v.roymenge@gmail.com>
2024-03-01 13:07:46 +0400

    licenses added

---
+++ assets/scripts/main.js
@@ -0,0 +1,18 @@
+// document.querySelector(".burgerMenu").addEventListener("click", () => {
+//     const nav = document.querySelector(".nav");
+//     if (nav.classList.contains("active")) {
+//         console.log(true)
+//         nav.classList.add("active");
+//     } else {
+//         nav.classList.remove("active");
+//     }
+// });
+const menu = document.querySelector(".burgerMenu"),
+    nav = document.querySelector(".nav");
+menu.addEventListener("click", () => {
+    if (nav.classList.contains('active')) {
+        nav.classList.remove("active");
+    } else {
+        nav.classList.add("active");
+    }
+})
